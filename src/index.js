477afe2aca986cb8f43143cfa3b610753b8851e7
Carlos Sosa <carloseduardososagonzalez@gmail.com>
2026-02-02 00:52:43 -0600

    feat: update url frontend

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: process.env.URL_FRONTEND || "http://localhost:3000",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
